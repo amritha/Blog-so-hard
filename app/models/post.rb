181aac3f2c10cfa6779eabc6f7d12dc3c31e24d4
Amritha <amrithap27@gmail.com>
2012-09-01 17:34:31 -0700

    Learned how to create a basic blog

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Base
+  attr_accessible :content, :name, :title
+
+  validates :name, :presence =>true
+  validates :title, :presence => true, :length => {:minimum => 5}
+  validates :content, :length => { :maximum => 250}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogsohard::Application.config.secret_token = 'ff6b751f5a593bc3180557bb3e08e4a13291ac90372b0ea06fac7ed757c36f8675039240e9272fd4a3da36781e68a53af98c6b802b5acf56a458e0c2dbf0714c'
